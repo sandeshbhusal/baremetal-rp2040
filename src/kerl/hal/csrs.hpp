83e4a5e232d57c8acba23b5e4f27ce332d4deed5
sandeshbhusal <mail.sandeshbhusal@gmail.com>
2026-02-07 18:16:53 -0500

    [feat]: Added a way to have nice CSRs in the code
    Signed-off-by: sandeshbhusal <mail.sandeshbhusal@gmail.com>

---
+++ src/kerl/hal/csrs.hpp
@@ -0,0 +1,80 @@
+#pragma once
+
+#include <cstdint>
+
+namespace kerl::hal::csr {
+
+/* Marker types for fields */
+struct ReadOnly {
+    static constexpr bool can_read = true;
+    static constexpr bool can_write = false;
+    static constexpr bool is_atomic = false;
+};
+
+struct WriteOnly {
+    static constexpr bool can_read = false;
+    static constexpr bool can_write = true;
+    static constexpr bool is_atomic = false;
+};
+
+struct ReadWrite {
+    static constexpr bool can_read = true;
+    static constexpr bool can_write = true;
+    static constexpr bool is_atomic = false;
+};
+
+struct AtomicWrite {
+    static constexpr bool can_read = true;
+    static constexpr bool can_write = true;
+    static constexpr bool is_atomic = true;
+};
+
+/**
+ * @brief: Register field access struct
+ */
+template <typename Policy, std::uintptr_t REG_ADDRESS, unsigned position,
+          unsigned width = 1>
+struct RegField {
+    static constexpr unsigned MASK = (1 << width) - 1;
+    /**
+     * @brief: Read-Modify-Write a register field
+     *
+     * @description: Only affects the space this field is occupying in the register
+     */
+    static void rmw(std::uint32_t value) {
+        static_assert(Policy::can_read && Policy::can_write,
+                      "Cannot either read/write to this reg");
+
+        auto set_value = read();
+        set_value = set_value & (~(MASK << position));
+        set_value |= (value & (MASK << position));
+
+        *(volatile uintptr_t*)(REG_ADDRESS) = set_value;
+    }
+
+    /**
+     * @brief: Read the register field value
+     */
+    static uint32_t read() {
+        static_assert(Policy::can_read, "Cannot read this reg");
+        return *((volatile uintptr_t*)(REG_ADDRESS)) & (MASK << position);
+    }
+
+    /**
+     * @brief: Set the register automically. The register needs to be atomic for
+     * this.
+     */
+    static void atomic_set(std::uint32_t value = 1) {
+        static_assert(Policy::is_atomic && Policy::can_write,
+                      "Not atomic/can't write to this reg");
+    }
+
+    /**
+     * @brief: Clear the registers atomically.
+     */
+    static void atomic_clear() {
+        static_assert(Policy::is_atomic && Policy::can_write,
+                      "Not atomic/can't clear this reg");
+    }
+};
+}  // namespace kerl::hal::csr
--- src/kerl/lib.cc
+++ src/kerl/lib.cc
@@ -1,3 +1,10 @@
+#include "hal/resets.hpp"
+
+using namespace kerl::hal::csr::resets;
+
 extern "C" void _start() {
+    Reset::IO_Bank0::atomic_set(0);
+    Reset::Pads_Bank0::atomic_set(0);
+
     while(1);
 }
